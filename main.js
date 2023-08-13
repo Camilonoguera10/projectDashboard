033923622ba155d466c4631fa0a99d3f57124829
Camilonoguera10 <camilonogueraa@gmail.com>
2023-08-13 14:58:51 -0500

    Integracion API

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-console.log("Hola");
-console.log("Hola");
-console.log("Hola");
-console.log("Hola catalogo");
+/*function api() {
+    fetch(`https://www.amdoren.com/api/currency.php?api_key=5L5JHA6T2sdbJaCHRUahefBbm6DyQ4&from=USD&to=EUR`,{method:"GET", mode:"cors"}).
+    then(resultado => resultado.json()).then(resultadodos => {console.log(resultadodos)})
+
+    fetch(`http://ucamp.alumnos.dev4humans.com.mx/Main/alumnos`,{method:"GET", mode:"cors"}).
+    then(resultado => resultado.json()).then(resultadodos => {console.log(resultadodos)})
+}
+
+
+api();*/
+
